366c930e933e6e2e5a82b27d4f0ea1a8e850ae4e
ragnarp12 <ragnarp12@ru.is>
2014-11-01 13:28:44 +0000

    Changed api route

--- routes/api.js
+++ routes/api.js
@@ -4,26 +4,31 @@
 var express = require('express');
 var router = express.Router();
 
+require('./models/kodemon')
+
+Entry = mongoose.model('Entry');
+
 /*
- * List all keys (without any values) that have been sent to the server. 
- * With the method you would see a list of all the methods 
+ * List all keys (without any values) that have been sent to the server.
+ * With the method you would see a list of all the methods
  * that have been sending messages to the server.
  */
-router.get('/listAll', function(req, res) {
-	res.send({'key': 'Nothing'});
+router.get('/keys', function(req, res) {
+    Entry.find
 });
 
 
 /* 
  * List all execution times for a given key.
-*/
-router.get('/listAllExecution/:key_id', function(req, res) {
-	var key_id = req.params.key_id;
+ */
+router.get('/key/:key_id/execute_time', function(req, res) {
+    var key_id = req.params.key_id;
 
-	res.send([{
-		'key': key_id}]);
-	//res.json([{'key': key_id}]);
-	/*process.nextTick(function () {
+    res.send([{
+        'key': key_id
+    }]);
+    //res.json([{'key': key_id}]);
+    /*process.nextTick(function () {
 		callback(null, [
 			{key: key_id}]
 			);
@@ -33,19 +38,19 @@ router.get('/listAllExecution/:key_id', function(req, res) {
 /*
  * List all execution times, for a given key on a given time range.
  */
-router.get('/listAllExecutionTime/:key_id/:time', function(req, res) {
-	var key_id = req.params.key_id;
-	var time_id = req.params.time;
+router.get('/key/:key_id/:time', function(req, res) {
+    var key_id = req.params.key_id;
+    var time_id = req.params.time;
 
-	res.send([{
-		'key': key_id,
-		'time': time_id
-		}]);
-	/*res.json([{
+    res.send([{
+        'key': key_id,
+        'time': time_id
+    }]);
+    /*res.json([{
 		'key': key_id,
 		'time': time_id
 		}]);*/
-	/*process.nextTick(function () {
+    /*process.nextTick(function () {
     callback(null, [
       {key: key_id, time: time_id }]
     );
