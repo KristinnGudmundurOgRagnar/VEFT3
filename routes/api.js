9acf26f7e58b54ea033e052a072f629bed52d6f3
gudmundurste12 <gudmundurste12@ru.is>
2014-11-01 13:47:01 +0000

    Implemented api/keys

--- routes/api.js
+++ routes/api.js
@@ -10,13 +10,41 @@ require('../models/kodemon')
 
 var Entry = mongoose.model('Entry');
 
+
+var connectToMongo = function(){
+    mongoose.connect("mongodb://localhost", {keepAlive: 1});
+    console.log("Connecting to mongoose");
+}
+
+var onMongoConnection = function(){
+    console.log("Connection to mongoose successful");
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+}
+
+mongoose.connection.on("disconnected", connectToMongo);
+mongoose.connection.on("connected", onMongoConnection);
+connectToMongo();
+
+
 /*
  * List all keys (without any values) that have been sent to the server.
  * With the method you would see a list of all the methods
  * that have been sending messages to the server.
  */
 router.get('/keys', function(req, res) {
-    Entry.find
+    Entry.find(function (err, entries) {
+        if (err)
+        {
+            console.error(err);
+            res.send(err);
+        }
+        else
+        {
+            console.log(entries)
+            res.send(entries);
+        }
+    })
 });
 
 
@@ -40,7 +68,7 @@ router.get('/key/:key_id/execute_time', function(req, res) {
 /*
  * List all execution times, for a given key on a given time range.
  */
-router.get('/key/:key_id/:time', function(req, res) {
+router.get('/key/:key_id/execute_time/:time', function(req, res) {
     var key_id = req.params.key_id;
     var time_id = req.params.time;
 
