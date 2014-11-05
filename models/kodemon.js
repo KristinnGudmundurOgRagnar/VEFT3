85062c74b3411afa74725ed7364e648af27b3e26
gudmundurste12 <gudmundurste12@ru.is>
2014-11-05 16:58:55 +0000

    Added elasticsearch

--- models/kodemon.js
+++ models/kodemon.js
@@ -1,11 +1,26 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+	mongoosastic = require("mongoosastic");
 
 var KodemonSchema = new Schema({
-    execution_time: Number,
-    timestamp: Number,
-    token: String,
-    key: String
+    execution_time: {type: Number, es_indexed: true},
+    timestamp: {type: Number, es_indexed: true},
+    token: {type: String, es_indexed: true},
+    key: {type: String, es_indexed: true}
 });
 
-mongoose.model('Entry', KodemonSchema);
+KodemonSchema.plugin(mongoosastic);
+
+var Entry = mongoose.model('Entry', KodemonSchema),
+stream = Entry.synchronize();
+
+Entry.createMapping(function(err, mapping){
+	if(err){
+		console.log("Error creating mapping(you can safely ignore this)");
+		console.log(err);
+	}
+	else{
+		console.log("Mapping created");
+		console.log(mapping);
+	}
+});
