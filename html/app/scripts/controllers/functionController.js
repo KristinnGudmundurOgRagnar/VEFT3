15d19fdc5e003e0894dae644ae1ef61b46e462f3
Kiddi <kristinnv12@ru.is>
2014-11-05 13:17:14 +0000

    reversed xaxis

--- html/app/scripts/controllers/functionController.js
+++ html/app/scripts/controllers/functionController.js
@@ -69,6 +69,9 @@ userApp.controller('functionController', [
                 title: {
                     text: 'Execution Times'
                 },
+                xAxis: {
+                reversed: true,
+                },
                 credits: {
                     enabled: false
                 },
